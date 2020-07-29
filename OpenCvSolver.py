e21b6a0035e1302ba97efbd4f0b2f32c6f6b8862
BoguckiMich <bogucki.mich@gmail.com>
2020-07-29 19:27:06 +0200

    secodn solution - RecustionSolver

--- OpenCvSolver.py
+++ OpenCvSolver.py
@@ -1,43 +1,56 @@
-import cv2
+import cv2, sys, argparse
 import numpy as np
 
-filename = "maze_01"
-img = cv2.imread('./mazes/'+filename+'.png')
-cv2.imshow('Maze', img)
 
-# to apply thresholding we need to convert picture to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Gray maze', gray)
+def main(options):
+    filename = options.filename
+    img = cv2.imread(filename)
+    cv2.imshow('Maze', img)
 
-# thresholding allows us to see where the pixel is different from white background, and inverting the colors
-ret, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    # to apply thresholding we need to convert picture to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Gray maze', gray)
 
-# saving to file
-cv2.imshow('Threshold 1', threshold)
+    # thresholding allows us to see where the pixel is different from white background, and inverting the colors
+    ret, threshold = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
 
-# we are looking for contours from entry point to exit point
-contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-drawnContours = cv2.drawContours(threshold, contours, 0, (255, 255, 255), 5)
-cv2.imshow('Contours1', drawnContours)
+    # saving to file
+    cv2.imshow('Threshold 1', threshold)
 
-# we are cleaning maze from unwanted contours
-drawnContours = cv2.drawContours(drawnContours, contours, 1, (0,0,0), 5)
-cv2.imshow('Contours2', drawnContours)
+    # we are looking for contours from entry point to exit point
+    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    drawnContours = cv2.drawContours(threshold, contours, 0, (255, 255, 255), 5)
+    cv2.imshow('Contours1', drawnContours)
 
-# we are creating buffer from contours
-contourBuffer = np.ones((19,19))
-contourBufferRes = cv2.dilate(drawnContours, contourBuffer, iterations=1)
-cv2.imshow('Buffer', contourBufferRes)
+    # we are cleaning maze from unwanted contours
+    drawnContours = cv2.drawContours(drawnContours, contours, 1, (0,0,0), 5)
+    cv2.imshow('Contours2', drawnContours)
 
-# by eroding we are creating same alement but smaller so we could later cut the line to the end
-thinBufferRes = cv2.erode(contourBufferRes, contourBuffer, iterations=1)
-cv2.imshow('Thin buffer', thinBufferRes)
+    # we are creating buffer from contours
+    contourBuffer = np.ones((19,19), np.uint8)
+    contourBufferRes = cv2.dilate(drawnContours, contourBuffer, iterations=1)
+    cv2.imshow('Buffer', contourBufferRes)
 
-# difference of two similar elements
-difference = cv2.absdiff(contourBufferRes, thinBufferRes)
-cv2.imshow('Absolute difference', difference)
+    # by eroding we are creating same alement but smaller so we could later cut the line to the end
+    thinBufferRes = cv2.erode(contourBufferRes, contourBuffer, iterations=1)
+    cv2.imshow('Thin buffer', thinBufferRes)
 
-#TODO find a way to draw it on picture
+    # difference of two similar elements
+    difference = cv2.absdiff(contourBufferRes, thinBufferRes)
+    cv2.imshow('Absolute difference', difference)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #TODO find a way to draw it on picture
+
+
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filename',
+        help='Image containing the maze.')
+    options = parser.parse_args()
+    sys.exit(main(options))
---
+++ RecursionSolver.py
@@ -0,0 +1,123 @@
+import sys, os, argparse, cv2
+from PIL import Image
+import numpy as np
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+
+# check if there is entrance on top or left side
+def getEntrance(image):
+    for x in range(1, image.size[0] - 1):
+        if image.getpixel((x, 1)) == white:
+            return x, 1
+    for y in range(1, image.size[1] - 1):
+        if image.getpixel((1, y)) == white:
+            return 1, y
+
+
+# check if there is entrance on top or left side
+def getExit(image):
+    for x in range(1, image.size[0] - 1):
+        if image.getpixel((x, image.size[1] - 2)) == white:
+            return x, image.size[1] - 2
+    for y in range(1, image.size[1] - 1):
+        if image.getpixel((image.size[0] - 2, y)) == white:
+            return image.size[0] - 2, y
+
+
+# check if we are still on the picture and we are not on the wall
+def isOnPath(image, x, y):
+    if image.size[0] > x >= 0 and image.size[1] > y >= 0:
+        if image.getpixel((x, y)) == white:
+            return True
+    return False
+
+
+"""
+finaly solultion with recursion, where we check:
+have we meet exit?
+if not check if on path?
+if on path - add red pixel, and check other pixels?
+if first met is ok, set as red and go next until met exit, if no exit go back to the loop
+"""
+
+#TODO add backtracking
+
+def solveMaze(image, x, y):
+    successful = False
+    if (x, y) == getExit(image):
+        successful = True
+    elif isOnPath(image, x, y):
+        # visited
+        image.putpixel((x, y), red)
+
+        successful = solveMaze(image, x + 1, y)
+        if not successful:
+            successful = solveMaze(image, x, y + 1)
+        if not successful:
+            successful = solveMaze(image, x - 1, y)
+        if not successful:
+            successful = solveMaze(image, x, y - 1)
+
+    # set proper track to green if successful
+    if successful:
+        image.putpixel((x, y), green)
+
+    return successful
+
+
+def main(options):
+    solved = False
+    sys.setrecursionlimit(7000)
+
+    image1 = cv2.imread(options.filename)
+    gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    # ret, threshold = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
+    canny_output = cv2.Canny(gray, 100, 255)
+    contours, hierarchy = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 4), dtype=np.uint8)
+    for i in range(len(contours)):
+        cv2.drawContours(drawing, contours, i, (255, 255, 255), options.thickness, cv2.LINE_8, hierarchy, 0)
+    # drawnContours = cv2.drawContours(threshold, contours, 0, (255, 255, 255), 5)
+    cv2.imshow('Contours', drawing)
+
+    ret, threshold = cv2.threshold(drawing, 200, 255, cv2.THRESH_BINARY_INV)
+    filename = os.path.splitext(options.filename)[0] + "_v2.png"
+    cv2.imwrite(filename, threshold)
+    cv2.imshow('threshold', threshold)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    imagePli = Image.open(filename)
+
+    imagePli = imagePli.convert('RGB')
+    x, y = getEntrance(imagePli)
+    print('Entrance at x = %d, y = %d' % (x, y))
+    solved = solveMaze(imagePli, x, y)
+    if not solved:
+        print('No solution exists.')
+    else:
+        print('Solved maze.')
+        basename = os.path.splitext(options.filename)[0]
+        imagePli.save(basename + '_solution' + '.png', 'PNG')
+
+    return 0 if solved else 1
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-th',
+        dest='thickness',
+        default=4,
+        type=int,
+        help='Change thickness of walls if the maze is unsolvable, 7-10 when walls thin, and 3-5 when walls are thicker')
+    parser.add_argument(
+        'filename',
+        help='Image containing the maze.')
+    options = parser.parse_args()
+    sys.exit(main(options))
